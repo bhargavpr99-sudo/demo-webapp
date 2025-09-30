2ccfaf306b00c6ac0827195ee775b70ab32f79d7
Bhargav <bhargavpr99@gmail.com>
2025-09-30 10:16:12 +0530

    Initial commit -demo webapp

---
+++ src/main/java/com/example/HelloServlet.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+        resp.setContentType("text/html");
+        resp.getWriter().println("<h1>Hello, World from HelloServlet!</h1>");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<html>
+  <head>
+    <title>Demo WebApp</title>
+  </head>
+  <body>
+    <h2>Welcome to Demo WebApp 🚀</h2>
+    <p><a href="hello">Click here to say Hello</a></p>
+  </body>
+</html>
